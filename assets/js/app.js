5b11e91873e23f8ca512cbfbf7311f00012a5594
Belloni-Marco-Camillo <96737807+Belloni-Marco-Camillo@users.noreply.github.com>
2022-04-20 15:55:37 +0200

    end_commit

---
+++ assets/js/app.js
@@ -0,0 +1,15 @@
+const app = new Vue({
+    el: '#app',
+    data: {
+        message: 'prova'
+    }
+})
+
+
+
+const app_img = new Vue({
+    el: "#app_img",
+    data: {
+        picture: 'https://picsum.photos/id/1005/600/200'
+    }
+})
